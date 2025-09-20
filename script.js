f0cd51860da58028e0d6fb4514a66fb307411fe8
thiyagarajan22j24-lab <thiyagarajan.22j24@kprcas.ac.in>
2025-09-20 12:42:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// Add interactivity to Contact form
+document.getElementById("btn").addEventListener("click", function() {
+  alert("Thank you for contacting me! Your message has been sent.");
+});
